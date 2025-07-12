4bef1f42e63220f6e36f17013b2f3d526f7587ad
morta1211morta1211 <108181586+morta1211morta1211@users.noreply.github.com>
2025-07-12 15:14:26 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+# ضع توكن البوت الخاص بك هنا بين علامتي التنصيص
+# يمكنك الحصول عليه من بوابة مطوري ديسكورد (Discord Developer Portal)
+BOT_TOKEN = "MTM3NTMyOTY5OTI1Njg2NDgyOA.G2QCzZ.Qci8JiLEfXgeeKsArGBvU817_mEi1L5U2JPSMM"
